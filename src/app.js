9701e64fd68559df7656ee5f98f190729e2df338
Francis Cirmi <cirmifrancis@gmail.com>
2024-06-13 21:38:19 -0300

    Actualizacion: diferencia entre mensaje propio y resto, avisa cuando alguien se conecta/desconecta y muestra el nro de usuarios conectados, scrollea automaticamente al haber un mensaje nuevo, width del container de mensajes fijado, error al poner el mismo nombre de un usuario ya conectado, entre otros...

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-//Importación de Módulos
+// Código del Server
+
+// Importación de Módulos
 const express = require("express");
 const exphbs = require("express-handlebars");
 const socket = require("socket.io");
@@ -6,37 +8,72 @@ const socket = require("socket.io");
 const app = express(); // Crea una instancia de la aplicación Express
 const PUERTO = 8080;
 
-//Middlewares
+// Middlewares
 app.use(express.static("./src/public")); // Cuando el servidor recibe una solicitud para un recurso estático, Express buscará en esta carpeta y servirá el archivo correspondiente si lo encuentra
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-//Configuramos Handlebars
+// Configuramos Handlebars
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 app.set("views", "./src/views"); // Establece la ubicación de las vistas (plantillas) de la aplicación
 
-//Rutas
+// Rutas
 app.get("/", (req, res) => {
     res.render("index");
 })
 
-//Listen
+// Listen
 const httpServer =  app.listen(PUERTO, () => {
     console.log(`Escuchando en el Puerto: ${PUERTO}`);
 })
 
-//Socket.io
+// Socket.io
 const io = new socket.Server(httpServer); // Crea una instancia de la aplicación Socket.io
 
 let messages = []; // Array de mensajes
+let usersConnected = {}; // Usuarios conectados
+let usersQuantity = 0; // Cantidad de usuarios
 
-io.on("connection", (socket) => { // Cuando el cliente envía un mensaje, se guarda en el array y se envía a todos los usuarios conectados
-    console.log("Nuevo usuario conectado");
-    io.emit("messagesLogs", messages);
-    
+io.on("connection", (socket) => {
+
+    socket.on("checkUser", (userName) => {
+        if (Object.values(usersConnected).includes(userName)) {
+            socket.emit("userExists");
+        } 
+        else {
+            socket.emit("userAccepted", userName);
+            usersConnected[socket.id] = userName;
+            
+            usersQuantity += 1;
+            console.log("Nuevo usuario conectado:", userName);
+
+            const welcomeMessage = { user: 'SERVIDOR', message: `El usuario <u>${userName}</u> se ha conectado.` };
+            messages.push(welcomeMessage);
+
+            io.emit("usersQuantity", usersQuantity);
+            io.emit("messagesLogs", messages);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        const userName = usersConnected[socket.id];
+        if (userName) {
+            usersQuantity -= 1;
+            console.log("Usuario desconectado:", userName);
+
+            const goodbyeMessage = { user: 'SERVIDOR', message: `El usuario <u>${userName}</u> se ha desconectado.` };
+            messages.push(goodbyeMessage);
+
+            delete usersConnected[socket.id];
+
+            io.emit("usersQuantity", usersQuantity);
+            io.emit("messagesLogs", messages);
+        }
+    });
+
     socket.on("message", data => { 
         messages.push(data);
         io.emit("messagesLogs", messages);
-    })
+    });
 })
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,21 +1,43 @@
+// Código del Cliente
+
 const socket = io(); // Creamos una instancia de socket.io desde el lado del cliente ahora
 
 let user; // Creamos una variable para guardar al usuario
 
 const chatBox = document.getElementById("chatBox");
+const usersQuantitySpan = document.getElementById("usersQuantity");
+const messagesContainer = document.querySelector(".container-messages");
 
-//Sweetalert2
-Swal.fire({
-    title: "Identificate",
-    text: "Ingresa un usuario para identificarte en el chat",
-    input: "text",
-    inputValidator: (value) => {
-        return !value && "Necesitas escribir un nombre para continuar";
-    },
-    allowOutsideClick: false,
-}).then( result => {
-    user = result.value.toUpperCase();
-})
+const promptUsername = () => { // Solicita el nombre de usuario
+    Swal.fire({
+        title: "Identificate",
+        text: "Ingresa un usuario para identificarte en el chat",
+        input: "text",
+        inputValidator: (value) => {
+            return !value && "Necesitas escribir un nombre para continuar.";
+        },
+        allowOutsideClick: false,
+    }).then(result => {
+        result.value && socket.emit("checkUser", result.value.toUpperCase());
+    });
+};
+
+promptUsername();
+
+socket.on("userExists", () => {
+    Swal.fire({
+        icon: "error",
+        title: "Nombre de usuario existente",
+        text: "Por favor, elige otro nombre de usuario.",
+        allowOutsideClick: false,
+    }).then(() => {
+        promptUsername();
+    });
+});
+
+socket.on("userAccepted", (username) => {
+    user = username;
+});
 
 //Mensaje a enviar
 chatBox.addEventListener("keyup", (event) => {
@@ -29,14 +51,34 @@ chatBox.addEventListener("keyup", (event) => {
 
 //Listener de mensajes
 socket.on("messagesLogs", data => {
-    const log = document.getElementById("messagesLogs");
     let messages = "";
 
     data.forEach( message => {
-        messages = messages + `<p> <b>${message.user}</b> dice: ${message.message} </p>`
+        const isCurrentUser = message.user === user;
+        const isServerMessage = message.user === "SERVIDOR";
+        const welcomeUser = message.message.includes(" conectado"); // Dejo un espacio para diferenciar de la subcadena 'desconectado'
+        const goodbyeUser = message.message.includes("desconectado");
+        
+        messages +=
+        `
+        <div ${ isServerMessage && welcomeUser ? 'class="server-message welcome-message"' : 
+                isServerMessage && goodbyeUser ? 'class="server-message goodbye-message"' :
+                isCurrentUser ? 'class="own-message"' : 'class="others-message"'}>
+            
+            <h3>${message.user}</h3>
+            <p>${message.message}</p>
+        </div>
+        `;
     })
-    log.innerHTML = messages;
+
+    messagesContainer.innerHTML = messages;
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
 })
+
+// Listener de cantidad de usuarios
+socket.on("usersQuantity", quantity => {
+    usersQuantitySpan.textContent = quantity;
+});
 
 //Mostrar Log de Mensajes
 socket.on("messagesLogs", (messages) => {
